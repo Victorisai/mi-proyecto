7a3988d9a021a3a2f46075cbf7a2580b1c460bfb
ISAI <isaicaceresramirez@gmail.com>
2025-07-01 12:27:50 -0500

    Actualice cambios

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,40 @@
-<footer>
+<footer class="site-footer">
     <div class="container">
-        <p>&copy; 2025 CedralSales. Todos los derechos reservados.</p>
-        <p><a href="contact.php">Contáctanos</a> | Tel: (52) 999-763-2818 | Email: info@cedralsales.com</p>
+        <div class="footer-grid">
+            <div class="footer-column">
+                <h3 class="footer-logo">VELORA</h3>
+                <p>Encuentra la propiedad de tus sueños. Te acompañamos en cada paso del camino.</p>
+            </div>
+
+            <div class="footer-column">
+                <h4>Explora</h4>
+                <ul>
+                    <li><a href="properties.php?category=terrenos">Terrenos</a></li>
+                    <li><a href="properties.php?category=casas">Casas</a></li>
+                    <li><a href="properties.php?category=departamentos">Departamentos</a></li>
+                    <li><a href="properties.php?category=desarrollos">Desarrollos</a></li>
+                    <li><a href="contact.php">Contacto</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-column">
+                <h4>Contáctanos</h4>
+                <p><i class="icon-phone"></i> (52) 999-763-2818</p>
+                <p><i class="icon-email"></i> <a href="mailto:info@cedralsales.com">info@cedralsales.com</a></p>
+                <p><i class="icon-location"></i> Cancún, Quintana Roo, México</p>
+            </div>
+
+            <div class="footer-column">
+                <h4>Síguenos</h4>
+                <div class="social-icons">
+                    <a href="#" class="social-icon" target="_blank"><img src="assets/images/iconcaracteristic/facebook-icon.png" alt="Facebook"></a>
+                    <a href="#" class="social-icon" target="_blank"><img src="assets/images/iconcaracteristic/instagram-icon.png" alt="Instagram"></a>
+                    <a href="#" class="social-icon" target="_blank"><img src="assets/images/iconcaracteristic/twitter-icon.png" alt="Twitter"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <p>&copy; <?php echo date("Y"); ?> VELORA. Todos los derechos reservados.</p>
     </div>
 </footer>
