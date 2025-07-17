5a6dbc4485d27081cc8fb4c14642621680ebdcdb
Victorisai <49794518+Victorisai@users.noreply.github.com>
2025-07-17 12:51:56 -0500

    Refactor remaining components to BEM

--- includes/header.php
+++ includes/header.php
@@ -1,49 +1,49 @@
 <header>
     <div class="container">
-        <div class="header-top">
+        <div class="header__top">
             <!-- Icono de menú hamburguesa -->
-            <div class="menu-toggle">
-                <span></span>
-                <span></span>
-                <span></span>
+            <div class="header__toggle">
+                <span class="header__toggle-line"></span>
+                <span class="header__toggle-line"></span>
+                <span class="header__toggle-line"></span>
             </div>
             <!-- Logo -->
-            <div class="logo">
+            <div class="header__logo">
                 <a href="index.php">
-                    <img src="assets/images/iconcaracteristic/logo-light.png" alt="Logo DOMABLY" class="logo-light">
-                    <img src="assets/images/iconcaracteristic/logo-dark.png" alt="Logo DOMABLY" class="logo-dark">
+                    <img src="assets/images/iconcaracteristic/logo-light.png" alt="Logo DOMABLY" class="header__logo--light">
+                    <img src="assets/images/iconcaracteristic/logo-dark.png" alt="Logo DOMABLY" class="header__logo--dark">
                 </a>
             </div>
         </div>
                 <!-- Línea separadora -->
-        <hr class="header-divider">
+        <hr class="header__divider">
         <!-- Nueva lista de navegación para enlaces fuera del menú hamburguesa -->
-        <ul class="header-nav">
-            <li><a href="properties.php?category=terrenos">Terrenos</a></li>
-            <li><a href="properties.php?category=casas">Casas</a></li>
-            <li><a href="properties.php?category=departamentos">Departamentos</a></li>
-            <li><a href="properties.php?category=desarrollos">Desarrollos</a></li>
-            <li><a href="properties.php?location=Benito Juárez">Cancun</a></li>
-            <li><a href="properties.php?location=Tulum">Tulum</a></li>
-            <li><a href="properties.php?location=Lázaro Cárdenas">Lázaro Cárdenas</a></li>
-            <li><a href="properties.php?location=Playa del Carmen">Playa del Carmen</a></li>
+        <ul class="header__nav">
+            <li class="header__nav-item"><a class="header__nav-link" href="properties.php?category=terrenos">Terrenos</a></li>
+            <li class="header__nav-item"><a class="header__nav-link" href="properties.php?category=casas">Casas</a></li>
+            <li class="header__nav-item"><a class="header__nav-link" href="properties.php?category=departamentos">Departamentos</a></li>
+            <li class="header__nav-item"><a class="header__nav-link" href="properties.php?category=desarrollos">Desarrollos</a></li>
+            <li class="header__nav-item"><a class="header__nav-link" href="properties.php?location=Benito Juárez">Cancun</a></li>
+            <li class="header__nav-item"><a class="header__nav-link" href="properties.php?location=Tulum">Tulum</a></li>
+            <li class="header__nav-item"><a class="header__nav-link" href="properties.php?location=Lázaro Cárdenas">Lázaro Cárdenas</a></li>
+            <li class="header__nav-item"><a class="header__nav-link" href="properties.php?location=Playa del Carmen">Playa del Carmen</a></li>
         </ul>
-        <hr class="header-divider-bottom">
-            <nav>
-                <ul>
-                    <li class="menu-header">
-                        <span class="menu-title">Tu propiedad ideal</span>
-                        <span class="close-menu">✕</span>
+        <hr class="header__divider">
+            <nav class="mobile-nav">
+                <ul class="mobile-nav__list">
+                    <li class="mobile-nav__header">
+                        <span class="mobile-nav__title">Tu propiedad ideal</span>
+                        <span class="mobile-nav__close-button">✕</span>
                     </li>
 
-                    <li><a href="index.php"><img src="assets/images/iconcaracteristic/home-icon.png" alt="Inicio Icon" class="nav-icon">Inicio</a></li>
-                    <li><a href="properties.php"><img src="assets/images/iconcaracteristic/properties-icon.png" alt="Propiedades Icon" class="nav-icon">Propiedades</a></li>
-                    <li><a href="contact.php"><img src="assets/images/iconcaracteristic/contact-icon.png" alt="Contacto Icon" class="nav-icon">Contacto</a></li>
-                    <li><a href="about.php"><img src="assets/images/iconcaracteristic/about-icon.png" alt="Sobre Nosotros Icon" class="nav-icon">Sobre Nosotros</a></li>
-                    <li><a href="join.php"><img src="assets/images/iconcaracteristic/join-icon.png" alt="Únete a Nosotros Icon" class="nav-icon">Únete a Nosotros</a></li>
-                    <li><a href="admin/index.php"><img src="assets/images/iconcaracteristic/account-icon.png" alt="Mi Cuenta Icon" class="nav-icon">Mi Cuenta</a></li>
+                    <li class="mobile-nav__item"><a class="mobile-nav__link" href="index.php"><img src="assets/images/iconcaracteristic/home-icon.png" alt="Inicio Icon" class="mobile-nav__icon">Inicio</a></li>
+                    <li class="mobile-nav__item"><a class="mobile-nav__link" href="properties.php"><img src="assets/images/iconcaracteristic/properties-icon.png" alt="Propiedades Icon" class="mobile-nav__icon">Propiedades</a></li>
+                    <li class="mobile-nav__item"><a class="mobile-nav__link" href="contact.php"><img src="assets/images/iconcaracteristic/contact-icon.png" alt="Contacto Icon" class="mobile-nav__icon">Contacto</a></li>
+                    <li class="mobile-nav__item"><a class="mobile-nav__link" href="about.php"><img src="assets/images/iconcaracteristic/about-icon.png" alt="Sobre Nosotros Icon" class="mobile-nav__icon">Sobre Nosotros</a></li>
+                    <li class="mobile-nav__item"><a class="mobile-nav__link" href="join.php"><img src="assets/images/iconcaracteristic/join-icon.png" alt="Únete a Nosotros Icon" class="mobile-nav__icon">Únete a Nosotros</a></li>
+                    <li class="mobile-nav__item"><a class="mobile-nav__link" href="admin/index.php"><img src="assets/images/iconcaracteristic/account-icon.png" alt="Mi Cuenta Icon" class="mobile-nav__icon">Mi Cuenta</a></li>
                 </ul>
             </nav>
     </div>
-    <div id="page-overlay"></div>
+    <div class="mobile-nav__overlay"></div>
 </header>
